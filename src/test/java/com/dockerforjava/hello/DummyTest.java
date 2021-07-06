743d1495cd1d541e4f3e9af7f96a2876afa06a56
Janice-Monteiro <26janice.monteiro@gmail.com>
2021-07-07 00:17:51 +0530

    Commit 1

---
+++ src/test/java/com/dockerforjava/hello/DummyTest.java
@@ -0,0 +1,13 @@
+package com.dockerforjava.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void aTest() {
+    assertEquals(true, true);
+  }
+}
